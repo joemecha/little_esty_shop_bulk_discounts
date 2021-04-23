366adc6132a4806d573f8b8edadcdff5aae6daee
joemecha <jwmecha@gmail.com>
2021-04-23 08:29:50 -0500

    Add model tests for BulkDiscount

---
+++ spec/models/bulk_discount_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe BulkDiscount do
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :quantity_threshold }
+    it { should validate_presence_of :percentage_discount }
+  end
+  describe "relationships" do
+    it { should belong_to :merchant }
+    it { should have_many(:items).through(:merchant) }
+    it {should have_many(:invoice_items).through(:items)}
+    it { should have_many(:invoices).through(:invoice_items) }
+  end
