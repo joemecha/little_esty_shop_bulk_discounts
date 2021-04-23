20844f0643cd7aeffd6945ad4f5fc0d28a8e0e2a
joemecha <jwmecha@gmail.com>
2021-04-23 18:42:15 -0500

    Add discount index link test and link in merchant dashboard

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -12,30 +12,36 @@ RSpec.describe 'merchant dashboard' do
     @customer_6 = Customer.create!(first_name: 'Herber', last_name: 'Coon')
 
     @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2)
-    @invoice_2 = Invoice.create!(customer_id: @customer_1.id, status: 2)
-    @invoice_3 = Invoice.create!(customer_id: @customer_2.id, status: 2)
-    @invoice_4 = Invoice.create!(customer_id: @customer_3.id, status: 2)
-    @invoice_5 = Invoice.create!(customer_id: @customer_4.id, status: 2)
-    @invoice_6 = Invoice.create!(customer_id: @customer_5.id, status: 2)
-    @invoice_7 = Invoice.create!(customer_id: @customer_6.id, status: 1)
+    @invoice_2 = Invoice.create!(customer_id: @customer_2.id, status: 2)
+    @invoice_3 = Invoice.create!(customer_id: @customer_3.id, status: 2)
+    @invoice_4 = Invoice.create!(customer_id: @customer_4.id, status: 2)
+    @invoice_5 = Invoice.create!(customer_id: @customer_5.id, status: 2)
+    @invoice_6 = Invoice.create!(customer_id: @customer_6.id, status: 2)
 
     @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id)
     @item_2 = Item.create!(name: "Conditioner", description: "This makes your hair shiny", unit_price: 8, merchant_id: @merchant1.id)
-    @item_3 = Item.create!(name: "Brush", description: "This takes out tangles", unit_price: 5, merchant_id: @merchant1.id)
+    @item_3 = Item.create!(name: "Brush", description: "This takes out tangles", unit_price: 20, merchant_id: @merchant1.id)
     @item_4 = Item.create!(name: "Hair tie", description: "This holds up your hair", unit_price: 1, merchant_id: @merchant1.id)
+    @item_5 = Item.create!(name: "Hair clippers", description: "This shaves your head", unit_price: 100, merchant_id: @merchant1.id)
+    @item_6 = Item.create!(name: "Almers Glue", description: "This holds up your mohawk", unit_price: 5, merchant_id: @merchant1.id)
 
+    # Note: customer ranking => customer6 110, customer5 100, customer2 40, customer3 20, customer1 10, customer4 1 (6, 5, 2, 3, 1, 4)
     @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 1, unit_price: 10, status: 0)
-    @ii_2 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_2.id, quantity: 1, unit_price: 8, status: 0)
-    @ii_3 = InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 2)
-    @ii_4 = InvoiceItem.create!(invoice_id: @invoice_3.id, item_id: @item_4.id, quantity: 1, unit_price: 5, status: 1)
+    @ii_2 = InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_2.id, quantity: 5, unit_price: 8, status: 0)
+    @ii_3 = InvoiceItem.create!(invoice_id: @invoice_3.id, item_id: @item_3.id, quantity: 1, unit_price: 20, status: 2)
+    @ii_4 = InvoiceItem.create!(invoice_id: @invoice_4.id, item_id: @item_4.id, quantity: 1, unit_price: 1, status: 1)
+    @ii_5 = InvoiceItem.create!(invoice_id: @invoice_5.id, item_id: @item_5.id, quantity: 1, unit_price: 100, status: 1)
+    @ii_6 = InvoiceItem.create!(invoice_id: @invoice_6.id, item_id: @item_6.id, quantity: 2, unit_price: 5, status: 1)
+    @ii_7 = InvoiceItem.create!(invoice_id: @invoice_6.id, item_id: @item_5.id, quantity: 1, unit_price: 100, status: 1)
 
     @transaction1 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_1.id)
-    @transaction2 = Transaction.create!(credit_card_number: 230948, result: 1, invoice_id: @invoice_3.id)
-    @transaction3 = Transaction.create!(credit_card_number: 234092, result: 1, invoice_id: @invoice_4.id)
-    @transaction4 = Transaction.create!(credit_card_number: 230429, result: 1, invoice_id: @invoice_5.id)
-    @transaction5 = Transaction.create!(credit_card_number: 102938, result: 1, invoice_id: @invoice_6.id)
-    @transaction6 = Transaction.create!(credit_card_number: 879799, result: 1, invoice_id: @invoice_7.id)
-    @transaction7 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_2.id)
+    @transaction2 = Transaction.create!(credit_card_number: 230948, result: 1, invoice_id: @invoice_2.id)
+    @transaction3 = Transaction.create!(credit_card_number: 234092, result: 1, invoice_id: @invoice_3.id)
+    @transaction4 = Transaction.create!(credit_card_number: 230429, result: 1, invoice_id: @invoice_4.id)
+    @transaction5 = Transaction.create!(credit_card_number: 102938, result: 1, invoice_id: @invoice_5.id)
+    @transaction5_2 = Transaction.create!(credit_card_number: 102938, result: 1, invoice_id: @invoice_5.id)
+    @transaction6 = Transaction.create!(credit_card_number: 879799, result: 1, invoice_id: @invoice_6.id)
+    @transaction7 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_6.id)
 
     visit merchant_dashboard_index_path(@merchant1)
   end
@@ -60,28 +66,36 @@ RSpec.describe 'merchant dashboard' do
     expect(current_path).to eq("/merchant/#{@merchant1.id}/invoices")
   end
 
+  it 'can see a link to my merchant discount index' do
+    expect(page).to have_link("Discounts")
+
+    click_link "Discounts"
+
+    expect(current_path).to eq("/merchant/#{@merchant1.id}/discounts")
+  end
+
   it 'shows the names of the top 5 customers with successful transactions' do
+    within("#customer-#{@customer_6.id}") do
+      expect(page).to have_content(@customer_6.full_name)
+      expect(page).to have_content(4)
+    end
+    within("#customer-#{@customer_5.id}") do
+      expect(page).to have_content(@customer_5.full_name)
+      expect(page).to have_content(2)
+    end
+    within("#customer-#{@customer_2.id}") do
+      expect(page).to have_content(@customer_2.full_name)
+      expect(page).to have_content(1)
+    end
+    within("#customer-#{@customer_3.id}") do
+      expect(page).to have_content(@customer_3.full_name)
+      expect(page).to have_content(1)
+    end
     within("#customer-#{@customer_1.id}") do
       expect(page).to have_content(@customer_1.full_name)
-      expect(page).to have_content(3)
+      expect(page).to have_content(1)
     end
-    # within("#customer-#{@customer_3.id}") do
-    #   expect(page).to have_content(@customer_3.full_name)
-    #   expect(page).to have_content(1)
-    # end
-    # within("#customer-#{@customer_2.id}") do
-    #   expect(page).to have_content(@customer_2.full_name)
-    #   expect(page).to have_content(1)
-    # end
-    # # within("#customer-#{@customer_3.id}") do
-    # #   expect(page).to have_content(@customer_3.full_name)
-    # #   expect(page).to have_content(1)
-    # # end
-    # within("#customer-#{@customer_4.id}") do
-    #   expect(page).to have_content(@customer_4.full_name)
-    #   expect(page).to have_content(1)
-    # end
-    # expect(page).to_not have_content(@customer_5.full_name)
+    expect(page).to_not have_content(@customer_4.full_name)
   end
   it "can see a section for Items Ready to Ship with list of names of items ordered and ids" do
     within("#items_ready_to_ship") do
