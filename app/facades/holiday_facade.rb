879c0e4a5848b737b801a96a1485877c9cd003ea
joemecha <jwmecha@gmail.com>
2021-04-27 21:38:53 -0500

    Remove unneeded files

--- app/facades/holiday_facade.rb
+++
@@ -1,8 +0,0 @@
-# class HolidayFacade
-  # def self.holidays(?)
-  #   json = JSON.parse(response.body, symbolize_names: true)
-  #   @holidays = json[:results].map do |holiday_attrs|
-  #     Holiday.new(holiday_attrs)
-  #   end
-  # end
-# end
